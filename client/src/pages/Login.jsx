63bfb54d253a7156ac632c1285bb641a875096ca
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-06-08 15:40:54 +0300

    update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function login() {
-  return <div>login</div>;
+  return (
+    <div className="login-Register-page">
+      <h1>WELCOME</h1>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+        <p>This is an error</p>
+        <span>
+          Don't have an account <Link to={"/Register"}>Register</Link>
+        </span>
+      </form>
+    </div>
+  );
 }
 
 export default login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="login-Register-page">
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder="Username" />
+        <input required type="password" placeholder="Password" />
+        <input required type="email" placeholder="Email Address" />
+        <button>Login</button>
+        <p>This is an error</p>
+        <span>
+          have an account?<Link to={"/Login"}>Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
